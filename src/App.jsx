07622067c983ce36a81c7a5cd0e2fc9ad90b9513
Maninder Singh <singhmaninder93899@gmail.com>
2025-05-29 22:02:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Work from './components/Work'
-import Stripes from './components/Stripes'
-import Products from './components/Products'
-import Marquees from './components/Marquees'
-import Cards from './components/Cards'
-import Footer from './components/Footer'
+import React, { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Work from './components/Work';
+import Stripes from './components/Stripes';
+import Products from './components/Products';
+import Marquees from './components/Marquees';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 import LocomotiveScroll from 'locomotive-scroll';
 
-
-
 function App() {
-  const locomotiveScroll = new LocomotiveScroll();
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+    });
+
+    return () => scroll.destroy();
+  }, []);
+
   return (
-    <div className='bg-zinc-900 text-white w-full'style={{ fontFamily: 'Satoshi, sans-serif' }}>
-    <Navbar />
-    <Work />
-    <Stripes />
-    <Products />
-    <Marquees />
-    <Cards />
-    <Footer />
+    <div className='bg-zinc-900 text-white w-full' style={{ fontFamily: 'Satoshi, sans-serif' }} data-scroll-container>
+      <Navbar />
+      <Work />
+      <Stripes />
+      <Products />
+      <Marquees />
+      <Cards />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
